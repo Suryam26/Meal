04152bfa191380c20670e3bb9c4572b3b50126db
Suryam26 <suryam.jain@gmail.com>
2020-07-16 22:10:25 +0530

    Home page done

---
+++ src/public/public.routes.js
@@ -0,0 +1,21 @@
+(function() {
+'use strict';
+
+angular.module('public')
+  .config(routeConfig);
+
+routeConfig.$inject = ['$stateProvider'];
+function routeConfig($stateProvider) {
+
+  $stateProvider
+    .state('public', {
+      abstract: true,
+      templateUrl: 'src/public/public.html'
+    })
+
+    .state('public.home', {
+      url: '/',
+      templateUrl: 'src/public/home/home.html'
+    });
+}
+}());
