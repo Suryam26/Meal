8d2d3e8517d84149d2e1d07b2e38ea180915eadf
Suryam26 <suryam.jain@gmail.com>
2020-07-17 12:20:17 +0530

    Loading spinner activated

--- src/common/common.module.js
+++ src/common/common.module.js
@@ -1,7 +1,14 @@
 (function() {
 'use strict';
 
-angular.module('common', []);
+angular.module('common', [])
+  .config(config);
+
+config.$inject = ['$httpProvider'];
+function config($httpProvider) {
+  $httpProvider.interceptors.push('loadingHttpInterceptor');
+  
+}
 
 
 }());
---
+++ src/common/loading/loading.interceptor.js
@@ -0,0 +1,40 @@
+(function() {
+'use strict';
+
+angular.module('common')
+  .factory('loadingHttpInterceptor', LoadingHttpInterceptor);
+
+
+LoadingHttpInterceptor.$inject = ['$rootScope', '$q'];
+function LoadingHttpInterceptor($rootScope, $q) {
+  var loadingCount = 0;
+  var eventName = 'spinner: activate';
+
+  return {
+    request: function (config) {
+      if (++loadingCount === 1) {
+        $rootScope.$broadcast(eventName, {on: true});
+      }
+      return config;
+    },
+
+
+    response: function (response) {
+      if (--loadingCount === 0) {
+        $rootScope.$broadcast(eventName, {on: false});
+      }
+      return response;
+    },
+
+    responseError: function (response) {
+      if (--loadingCount === 0) {
+        $rootScope.$broadcast(eventName, {on: false});
+      }
+      return $q.reject(response);
+    }
+
+  };
+
+}
+
+}());
